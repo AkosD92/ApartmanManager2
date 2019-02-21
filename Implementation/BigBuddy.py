f7f1e6abfcf5f0c0bce0a672d425a05c26ca7fe3
AkosD92 <35812730+AkosD92@users.noreply.github.com>
2019-02-21 18:50:48 +0100

    Registration form added, bootstrap included

--- Implementation/BigBuddy.py
+++ Implementation/BigBuddy.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from myForms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '5b930b141aa659fc8bcc08a37957e1db'
 
 latestForum = {
         'title' : "New entry from the forum",
@@ -22,4 +25,11 @@ latestQuestion = {
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('index.html', latestForum=latestForum, latestQuestion=latestQuestion)
+        form = LoginForm()
+        return render_template('index.html', latestForum=latestForum, latestQuestion=latestQuestion, form=form)
+
+
+@app.route("/register")
+def register():
+        form = RegistrationForm()
+        return render_template('register.html', form=form)
