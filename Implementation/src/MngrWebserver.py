2f6bad50a9dd813ffc6eb4135acf673b7c2d7c51
AkosD92 <35812730+AkosD92@users.noreply.github.com>
2020-11-16 22:59:56 +0100

    Fix template routes

--- Implementation/src/MngrWebserver.py
+++ Implementation/src/MngrWebserver.py
@@ -2,11 +2,12 @@ import os
 
 from flask import Flask
 from flask import render_template
+from flask import url_for
 
-TEMPLATE_DIR = os.path.abspath('./templates')
+TEMPLATE_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__))) + r'\templates'
+
 
 print(TEMPLATE_DIR)
-
 app = Flask(__name__, template_folder=TEMPLATE_DIR)
 
 @app.route('/')
@@ -16,4 +17,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
